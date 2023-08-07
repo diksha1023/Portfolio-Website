ef9aacf99137b63815ff0ab61605431160c235d5
Diksha <singhdi@iu.edu>
2023-08-07 18:59:01 -0400

    Added Home section - intro
    Home Section - intro added

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import "./Home.css";
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
-  return (
-    <div>Home Section here</div>
-  )
+    return (
+        <div className='container-fluid home'>
+
+            <div className='container home-content'>
+                <h1>Hi I'm</h1>
+                <h3>
+                    {/* Update your skills here */}
+                    <Typewriter
+                        options={{
+                            strings: ['Full Stack Software Developer', 
+                            'Java Developer',
+                            'Web Developer',
+                            'MERN Stack Developer',
+                            'Cloud Developer'
+
+                    ],
+                            autoStart: true,
+                            loop: true,
+                            delay: 5
+                        }}
+                    />
+                </h3>
+
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Home;
